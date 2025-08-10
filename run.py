854e2a49e0d615d50335f87ca52f9a3582dbb0fb
shrjawa <40277897+shrjawa@users.noreply.github.com>
2025-08-10 18:27:09 -0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+from src.pec import (
+    get_Q_h_c_A_b_penalty,
+    get_pauli_x_y_z_correlation_encoding_node_lists,
+    hamiltoninas_for_pauli_x_y_z_correlation_encoding,
+    run_QCBO
+)
+from src.step_1 import model_to_obj
